2d9975601f01e295ee2c5294a54ebfd3aee27fab
pakam2 <pkami.87@gmail.com>
2018-09-20 11:42:49 +0200

    Adding JS script to 'add_data.html', TODO: add 'active' class to clicked elements

--- hives/models.py
+++ hives/models.py
@@ -68,3 +68,174 @@ class FirstHiveDataModel(models.Model):
     motherBee = models.BooleanField(default=False)
     hive = models.ForeignKey(HiveModel, on_delete=models.CASCADE)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class SecondHiveDataModel(models.Model):
+
+    first_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    first_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    first_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    first_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    second_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    second_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    second_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    second_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    third_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    third_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    third_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    third_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    fourth_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fourth_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fourth_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fourth_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    fifth_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fifth_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fifth_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fifth_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    sixth_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    sixth_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    sixth_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    sixth_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    seventh_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    seventh_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    seventh_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    seventh_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    eight_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    eight_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    eight_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    eight_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    ninth_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    ninth_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    ninth_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    ninth_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    tenth_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    tenth_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    tenth_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    tenth_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    addDate = models.DateField(auto_now_add=True)
+    motherBee = models.BooleanField(default=False)
+    hive = models.ForeignKey(HiveModel, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class ThirdHiveDataModel(models.Model):
+
+    first_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    first_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    first_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    first_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    second_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    second_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    second_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    second_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    third_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    third_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    third_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    third_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    fourth_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fourth_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fourth_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fourth_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    fifth_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fifth_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fifth_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fifth_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    sixth_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    sixth_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    sixth_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    sixth_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    seventh_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    seventh_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    seventh_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    seventh_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    eight_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    eight_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    eight_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    eight_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    ninth_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    ninth_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    ninth_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    ninth_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    tenth_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    tenth_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    tenth_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    tenth_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    addDate = models.DateField(auto_now_add=True)
+    motherBee = models.BooleanField(default=False)
+    hive = models.ForeignKey(HiveModel, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class FourthHiveDataModel(models.Model):
+
+    first_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    first_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    first_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    first_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    second_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    second_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    second_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    second_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    third_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    third_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    third_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    third_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    fourth_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fourth_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fourth_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fourth_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    fifth_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fifth_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fifth_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    fifth_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    sixth_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    sixth_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    sixth_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    sixth_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    seventh_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    seventh_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    seventh_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    seventh_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    eight_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    eight_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    eight_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    eight_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    ninth_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    ninth_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    ninth_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    ninth_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    tenth_frame_honey = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    tenth_frame_speck = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    tenth_frame_beebread = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    tenth_frame_worm = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    addDate = models.DateField(auto_now_add=True)
+    motherBee = models.BooleanField(default=False)
+    hive = models.ForeignKey(HiveModel, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
